415cd576c396b187c5a7a5028730104fcc75c330
bhatamey <bhat_amey@yahoo.com>
2020-07-20 17:24:38 +0530

    linked DB

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 module.exports = {
-    database:  "mongodb://localhost:27017/meanAppDB",
+    database:  "mongodb+srv://amey:UEGSSbnnIjCUgSwK@cluster0-o10jq.mongodb.net/meanAppDB?retryWrites=true&w=majority",
+    //"mongodb://localhost:27017/meanAppDB",
     secret: "yoursecret",
 }
